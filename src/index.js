777b42036afe9e9f661e314b6c58d1e5042d7d0b
tanmayshende007@gmail.com <tanmayshende007@gmail.com>
2024-07-12 17:05:09 +0530

    Hashtag respository is created and also created bulk tags

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connect = require('./config/database')
+const {HashtagRepository}= require('./repository/index')
 
 const app = express();
 const PORT = 3000
@@ -8,4 +9,29 @@ app.listen(PORT, async () => {
     console.log(`Server is up at port ${PORT}`);
     await connect();
     console.log('DB is connected');
+
+    const tagRepo = new HashtagRepository();
+    const tags = tagRepo.bulkCreate([
+        {
+            title: 'Trend',
+            tweets: []
+        },
+        {
+            title: 'fun',
+            tweets: []
+        },
+        {
+            title: 'Coding',
+            tweets: []
+        },
+        {
+            title: 'Growth',
+            tweets: []
+        },
+        {
+            title: 'Life',
+            tweets: []
+        },
+    ]) 
+
 })
